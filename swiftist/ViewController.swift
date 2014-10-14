395a770c86a53dcc844c03c9b665b2e1bb029c9d
mountainKaku <mountain.kaku@gmail.com>
2014-10-14 11:16:42 +0900

    try Objects & Classes

--- swiftist/ViewController.swift
+++ swiftist/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController {
         trySampleValues()
         tryControlFlow()
         tryFunctionsRClosures()
+        tryObjectsRClasses()
     }
     
     func trySampleValues() {
@@ -211,6 +212,116 @@ class ViewController: UIViewController {
             let result = 3 * number
             return result
         }))
+    }
+
+    func tryObjectsRClasses() {
+        class Shape {
+            var numberOfSides = 0
+            func simpleDescription() -> String {
+                return "A shape with \(numberOfSides) sides."
+            }
+        }
+        var shape = Shape()
+        shape.numberOfSides = 7
+        var shapeDescription = shape.simpleDescription()
+        println("shapeDescription: " + shapeDescription)
+        
+        class NamedShape {
+            var numberOfSides: Int = 0
+            var name: String
+            
+            init(name: String) {
+                self.name = name
+            }
+            
+            func simpleDescription() -> String {
+                return "A shape with \(numberOfSides) sides."
+            }
+        }
+        
+        class Square: NamedShape {
+            var sideLength: Double
+            
+            init(sideLength: Double, name: String) {
+                self.sideLength = sideLength
+                super.init(name: name)
+                numberOfSides = 4
+            }
+            
+            func area() ->  Double {
+                return sideLength * sideLength
+            }
+            
+            override func simpleDescription() -> String {
+                return "A square with sides of length \(sideLength)."
+            }
+        }
+        let test = Square(sideLength: 5.2, name: "my test square")
+        println("Square \(test.area())")
+        println("Square simpleDescription: " + String(test.simpleDescription()))
+        
+        class EquilateralTriangle: NamedShape {
+            var sideLength: Double = 0.0
+            
+            init(sideLength: Double, name: String) {
+                self.sideLength = sideLength
+                super.init(name: name)
+                numberOfSides = 3
+            }
+            
+            var perimeter: Double {
+                get {
+                    return 3.0 * sideLength
+                }
+                set {
+                    sideLength = newValue / 3.0
+                }
+            }
+            
+            override func simpleDescription() -> String {
+                return "An equilateral triagle with sides of length \(sideLength)."
+            }
+        }
+        var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+        println("triangle.perimeter: + \(triangle.perimeter)")
+        triangle.perimeter = 9.9
+        println("triangle.sideLength: + \(triangle.sideLength)")
+        
+        class TriangleAndSquare {
+            var triangle: EquilateralTriangle {
+                willSet {
+                    square.sideLength = newValue.sideLength
+                }
+            }
+            var square: Square {
+                willSet {
+                    triangle.sideLength = newValue.sideLength
+                }
+            }
+            init(size: Double, name: String) {
+                square = Square(sideLength: size, name: name)
+                triangle = EquilateralTriangle(sideLength: size, name: name)
+            }
+        }
+        var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+        println("triangleAndSquare.square.sideLength: + \(triangleAndSquare.square.sideLength)")
+        println("triangleAndSquare.triangle.sideLength: + \(triangleAndSquare.triangle.sideLength)")
+        triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+        println("triangleAndSquare.triangle.sideLength: + \(triangleAndSquare.triangle.sideLength)")
+        
+        class Counter {
+            var count: Int = 0
+            func incrementBy(amount: Int, numberOfTimes times: Int) {
+                count += amount * times
+            }
+        }
+        var counter = Counter()
+        counter.incrementBy(2, numberOfTimes: 7)
+        println("counter.count:" + String(counter.count))
+        
+        let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+        let sideLength = optionalSquare?.sideLength
+         println("optionalSquare?.sideLength: + \(sideLength)")
     }
     
     func loadAddressURL() {
